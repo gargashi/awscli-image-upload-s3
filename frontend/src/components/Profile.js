2002361408d21c901aaf87790f37b789ff1d6d1c
Ashish Garg <45831857+gargashi@users.noreply.github.com>
2023-05-31 15:14:34 +0530

    Update Profile.js

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,20 +1,13 @@
-import { Box, Image, Text, VStack } from "@chakra-ui/react";
+import { Box, Text, VStack } from "@chakra-ui/react";
 import Posts from "./Posts";
 
 const Profile = () => {
   return (
     <Box>
       <VStack p={7} m="auto" width="fit-content" borderRadius={6} bg="gray.700">
-        <Image
-          borderRadius="full"
-          boxSize="80px"
-          src="https://bit.ly/kent-c-dodds"
-          alt="Profile"
-        />
-        <Text>Ashish Garg</Text>
-        <Text fontSize="lg" color="gray.400">
-          Software Engineer
-        </Text>
+        
+        <Text>Hello</Text>
+        
       </VStack>
 
       <Posts />
